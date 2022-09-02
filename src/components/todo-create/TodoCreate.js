e4588bcbc423270fc452c3d2b020247174f2878e
qkohst <kukohsantoso013@gmail.com>
2022-09-02 16:20:41 +0700

    Add Dinamis List Bay Input Value

--- src/components/todo-create/TodoCreate.js
+++ src/components/todo-create/TodoCreate.js
@@ -1,22 +1,32 @@
 import './TodoCreate.css'
+import {useState} from "react";
 
 const TodoCreate = (props)=>{
+
+    const [getInputTodo, setInputTodo] = useState('')
+
+    const handeInputTodo = (event)=>{
+        setInputTodo(event.target.value)
+        // console.log(getInputTodo)
+    }
 
     const handleSubmit = (event)=>{
         event.preventDefault()
 
         const newTodo = {
             id: Math.floor(Math.random() * 100) + 1,
-            title: 'lear react'
+            title: getInputTodo
         }
 
         props.onCreateTodo(newTodo)
+
+        setInputTodo('')
         // console.log(newTodo)
     }
 
     return (
         <form className="todo-form" onSubmit={handleSubmit}>
-            <input type="text" />
+            <input type="text" value={getInputTodo} onChange={handeInputTodo}/>
             <button type="submit">Add</button>
         </form>
     )
