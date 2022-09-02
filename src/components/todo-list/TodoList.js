389eccd408bade91954b153b213835166befac66
qkohst <kukohsantoso013@gmail.com>
2022-09-02 15:00:14 +0700

    Add Components

---
+++ src/components/todo-list/TodoList.js
@@ -0,0 +1,13 @@
+import './TodoList.css';
+
+const TodoList = ()=>{
+    return (
+        <ul>
+            <li>Learn React</li>
+            <li>Build Tools</li>
+            <li>Record</li>
+        </ul>
+    )
+}
+
+export default TodoList
---
+++ src/components/todo/Todo.js
@@ -0,0 +1,13 @@
+import './Todo.css';
+import TodoList from "../todo-list/TodoList";
+
+const Todo = ()=>{
+    return (
+        <div>
+            <h3>Todo List</h3>
+            <TodoList/>
+        </div>
+    )
+}
+
+export default Todo
